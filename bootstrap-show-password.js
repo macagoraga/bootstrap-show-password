5013555ed1c49c3050239a4ca84af261a86d2c88
macagoraga <info@macagoraga.com>
2014-10-23 07:50:04 +0200

    removed bootstrap v2, added font-awesome
     * Change by macagoraga
     * - Removed support for bootstrap v2
     * - Added support for font-awesome

--- bootstrap-show-password.js
+++ bootstrap-show-password.js
@@ -2,163 +2,138 @@
  * @author zhixin wen <wenzhixin2010@gmail.com>
  * https://github.com/wenzhixin/bootstrap-show-password
  * version: 1.0.1
+ *
+ * Change by macagoraga
+ * - Removed support for bootstrap v2
+ * - Added support for font-awesome
  */
 
 !function ($) {
 
-    'use strict';
+  'use strict';
 
-    // TOOLS DEFINITION
-    // ======================
+  // PASSWORD CLASS DEFINITION
+  // ======================
 
-    // it only does '%s', and return '' when arguments are undefined
-    var sprintf = function(str) {
-        var args = arguments,
-            flag = true,
-            i = 1;
+  var Password = function(element, options) {
+    this.options   = options;
+    this.$element  = $(element);
+    this.isShown = false;
 
-        str = str.replace(/%s/g, function () {
-            var arg = args[i++];
+    this.init();
+  };
 
-            if (typeof arg === 'undefined') {
-                flag = false;
-                return '';
-            }
-            return arg;
-        });
-        if (flag) {
-            return str;
-        }
-        return '';
-    };
+  Password.DEFAULTS = {
+    placement: 'after', // 'before' or 'after'
+    message: 'Click here to show/hide password'
+  };
 
-    // PASSWORD CLASS DEFINITION
-    // ======================
+  Password.prototype.init = function() {
+    var placementFuc;
 
-    var Password = function(element, options) {
-        this.options   = options;
-        this.$element  = $(element);
-        this.isShown = false;
+    if (this.options.placement === 'before') {
+      placementFuc = 'insertBefore';
+    } else {
+      this.options.placement = 'after'; // default to after
+      placementFuc = 'insertAfter';
+    }
 
-        this.init();
-    };
+    // Create the text, icon and assign
+    this.$element.wrap('<div class="input-group" />');
 
-    Password.DEFAULTS = {
-        placement: 'after', // 'before' or 'after'
-        white: false, // v2
-        message: 'Click here to show/hide password'
-    };
+    this.$text = $('<input type="text" />')
+      [placementFuc](this.$element)
+      .attr('class', this.$element.attr('class'))
+      .attr('placeholder', this.$element.attr('placeholder'))
+      .css('display', this.$element.css('display'))
+      .val(this.$element.val()).hide();
 
-    Password.prototype.init = function() {
-        var placementFuc,
-            inputClass; // v2 class
+    this.$icon = $([
+      '<span tabindex="100" title="' + this.options.message + '" class="input-group-addon">',
+      '<i class="fa fa-eye fa-lg fa-fw"></i>',
+      '</span>'
+    ].join(''))[placementFuc](this.$text).css('cursor', 'pointer');
 
-        if (this.options.placement === 'before') {
-            placementFuc = 'insertBefore';
-            inputClass = 'input-prepend';
-        } else {
-            this.options.placement = 'after'; // default to after
-            placementFuc = 'insertAfter';
-            inputClass = 'input-append';
-        }
+    // events
+    this.$text.off('keyup').on('keyup', $.proxy(function() {
+      this.$element.val(this.$text.val());
+    }, this));
 
-        // Create the text, icon and assign
-        this.$element.wrap(sprintf('<div class="%s input-group" />', inputClass));
+    this.$icon.off('click').on('click', $.proxy(function() {
+      this.$text.val(this.$element.val());
+      this.toggle();
+    }, this));
+  };
 
-        this.$text = $('<input type="text" />')
-            [placementFuc](this.$element)
-            .attr('class', this.$element.attr('class'))
-            .attr('placeholder', this.$element.attr('placeholder'))
-            .css('display', this.$element.css('display'))
-            .val(this.$element.val()).hide();
+  Password.prototype.toggle = function(_relatedTarget) {
+    this[!this.isShown ? 'show' : 'hide']();
+  };
 
-        this.$icon = $([
-            '<span tabindex="100" title="' + this.options.message + '" class="add-on input-group-addon">',
-            '<i class="icon-eye-open' + (this.options.white ? ' icon-white' : '') +
-                ' glyphicon glyphicon-eye-open"></i>',
-            '</span>'
-        ].join(''))[placementFuc](this.$text).css('cursor', 'pointer');
+  Password.prototype.show = function(_relatedTarget) {
+    var e = $.Event('show.bs.password', {relatedTarget: _relatedTarget});
+    this.$element.trigger(e);
 
-        // events
-        this.$text.off('keyup').on('keyup', $.proxy(function() {
-            this.$element.val(this.$text.val());
-        }, this));
+    this.isShown = true;
+    this.$element.hide();
+    this.$text.show();
+    this.$icon.find('i')
+      .removeClass('fa-eye')
+      .addClass('fa-eye-slash');
 
-        this.$icon.off('click').on('click', $.proxy(function() {
-            this.$text.val(this.$element.val());
-            this.toggle();
-        }, this));
-    };
+    // v3 input-group
+    this.$text[this.options.placement](this.$element);
+  };
 
-    Password.prototype.toggle = function(_relatedTarget) {
-        this[!this.isShown ? 'show' : 'hide']();
-    };
+  Password.prototype.hide = function(_relatedTarget) {
+    var e = $.Event('hide.bs.password', {relatedTarget: _relatedTarget});
+    this.$element.trigger(e);
 
-    Password.prototype.show = function(_relatedTarget) {
-        var e = $.Event('show.bs.password', {relatedTarget: _relatedTarget});
-        this.$element.trigger(e);
+    this.isShown = false;
+    this.$element.show();
+    this.$text.hide();
+    this.$icon.find('i')
+      .removeClass('fa-eye-slash')
+      .addClass('fa-eye');
 
-        this.isShown = true;
-        this.$element.hide();
-        this.$text.show();
-        this.$icon.find('i')
-            .removeClass('icon-eye-open glyphicon-eye-open')
-            .addClass('icon-eye-close glyphicon-eye-close');
-
-        // v3 input-group
-        this.$text[this.options.placement](this.$element);
-    };
-
-    Password.prototype.hide = function(_relatedTarget) {
-        var e = $.Event('hide.bs.password', {relatedTarget: _relatedTarget});
-        this.$element.trigger(e);
-
-        this.isShown = false;
-        this.$element.show();
-        this.$text.hide();
-        this.$icon.find('i')
-            .removeClass('icon-eye-close glyphicon-eye-close')
-            .addClass('icon-eye-open glyphicon-eye-open');
-
-        // v3 input-group
-        this.$element[this.options.placement](this.$text);
-    };
+    // v3 input-group
+    this.$element[this.options.placement](this.$text);
+  };
 
 
-    // PASSWORD PLUGIN DEFINITION
-    // =======================
+  // PASSWORD PLUGIN DEFINITION
+  // =======================
 
-    var old = $.fn.password;
+  var old = $.fn.password;
 
-    $.fn.password = function(option, _relatedTarget) {
-        return this.each(function() {
-            var $this = $(this),
-                data = $this.data('bs.password'),
-                options = $.extend({}, Password.DEFAULTS, $this.data(), typeof option === 'object' && option);
+  $.fn.password = function(option, _relatedTarget) {
+    return this.each(function() {
+      var $this = $(this),
+          data = $this.data('bs.password'),
+          options = $.extend({}, Password.DEFAULTS, $this.data(), typeof option === 'object' && option);
 
-            if (!data) {
-                $this.data('bs.password', (data = new Password(this, options)));
-            }
+      if (!data) {
+        $this.data('bs.password', (data = new Password(this, options)));
+      }
 
-            if (typeof option === 'string') {
-                data[option](_relatedTarget);
-            }
-        });
-    };
-
-    $.fn.password.Constructor = Password;
-
-
-    // PASSWORD NO CONFLICT
-    // =================
-
-    $.fn.password.noConflict = function() {
-        $.fn.password = old;
-        return this;
-    };
-    
-    $(function () {
-        $('[data-toggle="password"]').password();
+      if (typeof option === 'string') {
+        data[option](_relatedTarget);
+      }
     });
+  };
+
+  $.fn.password.Constructor = Password;
+
+
+  // PASSWORD NO CONFLICT
+  // =================
+
+  $.fn.password.noConflict = function() {
+    $.fn.password = old;
+    return this;
+  };
+
+  $(function () {
+    $('[data-toggle="password"]').password();
+  });
 
 }(window.jQuery);
